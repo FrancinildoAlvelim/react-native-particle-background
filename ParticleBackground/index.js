2538a67f700c07c67edba809abd1e1e3654cdb1e
Francinildo <franalves1923@gmail.com>
2021-01-10 20:58:27 -0300

    Added basic prop validation, testing multi colored particles feature, moving particle view generation loop out of render method

--- ParticleBackground/index.js
+++ ParticleBackground/index.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { View, ViewPropTypes } from "react-native";
 import Particle from "./components/Particle";
 import { randomIntFromInterval } from "./utils";
 
+// TO-DO consider create prop particleColors when user passes multiple particle color
 const ParticleBackground = ({
   particleSize,
   particleDispersion,
   particleColor,
   backgroundColor,
-  containerStyle
+  containerStyle,
 }) => {
   const [particles, setParticles] = useState([]);
   const [componentDimensions, setComponentDimensions] = useState({
     width: 0,
-    height: 0
+    height: 0,
   });
+
+  // TO-DO move random statement to some method
+  const getParticleColor = () =>{
+    const multiplePaticleColors = Array.isArray(particleColor)
+
+    if(!multiplePaticleColors) return particleColor
+
+    const particleColorLength = particleColor.length
+    const randomIndex = Math.floor(Math.random() * particleColorLength )
+    const randomlyPickedColor = particleColor[randomIndex]
+
+    return randomlyPickedColor
+  }
 
   const getParticles = () => {
     const currentParticles = [];
@@ -29,10 +44,24 @@ const ParticleBackground = ({
       currentParticles.push({
         y: realY,
         x: realX,
-        size: particleSize
+        size: particleSize,
       });
     }
     return currentParticles;
+  };
+
+  const getParticleViews = () => {
+    return particles.map(({ x, y, size }, index) => (
+      <Particle
+        parentWidth={componentDimensions.width}
+        parentHeight={componentDimensions.height}
+        color={getParticleColor()}
+        initialX={x}
+        initialY={y}
+        size={size}
+        key={index}
+      />
+    ));
   };
 
   const setDimensions = ({ nativeEvent }) => {
@@ -61,19 +90,17 @@ const ParticleBackground = ({
       style={[containerStyle, { backgroundColor }]}
       onLayout={setDimensions}
     >
-      {particles.map(({ x, y, size }, index) => (
-        <Particle
-          parentWidth={componentDimensions.width}
-          parentHeight={componentDimensions.height}
-          color={particleColor}
-          initialX={x}
-          initialY={y}
-          size={size}
-          key={index}
-        />
-      ))}
+      {getParticleViews()}
     </View>
   );
+};
+
+ParticleBackground.propTypes = {
+  particleSize: PropTypes.number,
+  particleDispersion: PropTypes.number,
+  particleColor: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  backgroundColor: PropTypes.string,
+  containerStyle: ViewPropTypes.style,
 };
 
 export default ParticleBackground;
